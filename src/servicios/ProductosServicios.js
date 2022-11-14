7d1338d616ac80d7c62c4d0d41bbf22e9ecd071a
JHernandez435 <69222698+JHernandez435@users.noreply.github.com>
2022-11-14 00:01:36 -0500

    .

--- src/servicios/ProductosServicios.js
+++ src/servicios/ProductosServicios.js
@@ -2,39 +2,39 @@ import axios from "axios";
 
 const ProductosServicios = {}
 // const URL = "http://localhost:8000/api/productos/";
-const URI = "https://omegapetshop-api.herokuapp.com";
+const URI = "https://omegapetshop-api.herokuapp.com/";
 
     
 ProductosServicios.listarProductos = () => {
-    return axios.get(URI);
+    return axios.get(URI+"api/produtos");
 }
 
 ProductosServicios.listarProductosDisponibles = () => {
-    return axios.get(URI+"?disp=true");
+    return axios.get(URI+"api/produtos?disp=true");
 }
 
 ProductosServicios.filtrarProductos = (criterio) => {
-    return axios.get(URI+"?q="+criterio);
+    return axios.get(URI+"api/produtos?q="+criterio);
 }
 
 ProductosServicios.filtrarProductosDisponibles = (criterio) => {
-    return axios.get(URI+"?q="+criterio+"&disp=true");
+    return axios.get(URI+"api/produtos?q="+criterio+"&disp=true");
 }
 
 ProductosServicios.cargarProducto = (id) => {
-    return axios.get(URI + id);
+    return axios.get(URI+ "api/produtos" + id);
 }
 
 ProductosServicios.guardarProducto = (producto) => {
-    return axios.post(URI, producto);
+    return axios.post(URI, +"api/produtos"+ producto);
 }
 
 ProductosServicios.modificarProducto = (id, producto) => {
-    return axios.put(URI + id, producto);
+    return axios.put(URI + "api/produtos" + id, producto);
 }
 
 ProductosServicios.borrarProducto = (id) => {
-    return axios.delete(URI + id);
+    return axios.delete(URI + "api/produtos" + id);
 }
 
 export default ProductosServicios;
